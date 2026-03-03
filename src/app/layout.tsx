a7af2fb10dedcb30e10065373b320e513c0e3555
phxbinh <phxbinh@gmail.com>
2026-03-03 12:39:58 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -469,7 +469,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import { Toast } from "@/components/Toast";
-import Sidebar from "@/components/siderbar/Sidebar";
+import Sidebar from "@/components/sidebar/Sidebar";
 import { Menu, X } from "lucide-react";
 
 const inter = Inter({ subsets: ["latin"] });
