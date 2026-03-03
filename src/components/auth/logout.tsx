493caf643cd669580a66473bed25a9ff06fb0b38
phxbinh <phxbinh@gmail.com>
2026-03-03 18:03:47 +0700

    Update logout.tsx

--- src/components/auth/logout.tsx
+++ src/components/auth/logout.tsx
@@ -1,23 +1,4 @@
 // src/components/auth/logout.tsx
-/*
-'use client'
-import { signOut } from '@/lib/authActions/auth';
-
-export function LogoutButton() {
-  return (
-    <form action={signOut}>
-      <button
-        type="submit"
-        className="px-4 py-2 rounded bg-red-600 text-white hover:bg-red-500"
-      >
-        Logout
-      </button>
-    </form>
-  );
-}
-*/
-
-
 'use client'
 import { signOut } from '@/lib/authActions/auth';
 import { LogOut } from 'lucide-react';
