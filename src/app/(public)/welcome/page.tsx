a57538bd34283d75962b0754d151afc8c722b988
phxbinh <phxbinh@gmail.com>
2026-03-03 16:51:21 +0700

    Create page.tsx

---
+++ src/app/(public)/welcome/page.tsx
@@ -0,0 +1,19 @@
+// src/app/(public)/welcome/page.tsx
+import { createSupabaseServerClient } from '@/lib/supabase/server';
+import { redirect } from 'next/navigation';
+import { LogoutButton } from '@/components/store/logoutButton';
+
+export default async function WelcomePage() {
+  const supabase = await createSupabaseServerClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) redirect('/login');
+
+  return (<>
+    <h1>Welcome {user.email}</h1>
+    < LogoutButton />
+    </>);
+}
