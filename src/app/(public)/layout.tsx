7b9a26d21d52cbe8e8142ddb456259875ed94438
phxbinh <phxbinh@gmail.com>
2026-03-04 23:26:15 +0700

    Update layout.tsx

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,4 +1,12 @@
 // src/app/(public)/layout.tsx
+"use client";
+
+import { useState } from "react";
+import { ThemeToggle } from "@/components/ThemeToggle";
+import { Toast } from "@/components/Toast";
+import Sidebar from "@/components/sidebar/Sidebar";
+import { Menu, X } from "lucide-react";
+import Link from "next/link";
 
 export default function PublicLayout({
   children,
