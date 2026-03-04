a3978f139338b26613de734ea4a8d4b59dc54b56
phxbinh <phxbinh@gmail.com>
2026-03-04 16:18:24 +0700

    Update layout.tsx

--- src/app/(auth)/dashboard/layout.tsx
+++ src/app/(auth)/dashboard/layout.tsx
@@ -1,33 +1,6 @@
 
-/*
-import DashboardSidebar from '@/components/dashboard/DashboardSidebar';
-import DashboardHeader from '@/components/dashboard/DashboardHeader';
-import { getCurrentUser } from '@/lib/authActions/getUser';
-
-export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
-  const user = await getCurrentUser();
-
-  return (
-    <div className="flex h-full w-full overflow-hidden">
-      
-      <DashboardSidebar user={user} />
-
-      <div className="flex-1 flex flex-col min-w-0 h-full overflow-hidden">
-        <DashboardHeader onMenuClick={() => {}} />
-        
-        
-        <main className="flex-1 overflow-y-auto custom-scrollbar bg-transparent p-4 md:p-8">
-          <div className="max-w-6xl mx-auto w-full">
-            {children}
-          </div>
-        </main>
-      </div>
-    </div>
-  );
-}
-*/
-
-
+// src/app/(auth)/dashboard/layout.tsx
+// làm việc trên tài khoản của user
 
 import { getCurrentUser } from '@/lib/authActions/getUser';
 import DashboardShell from '@/components/dashboard/DashboardShell';
