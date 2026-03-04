477e2d57bccf954104501959a1cc8db3c0f866c0
phxbinh <phxbinh@gmail.com>
2026-03-04 16:04:18 +0700

    Update profiles.ts

--- src/lib/sqlQueries/profiles.ts
+++ src/lib/sqlQueries/profiles.ts
@@ -1,5 +1,5 @@
 // src/lib/dataQueries/profiles.ts
-import { sql, sqlAdmin, sqlApp } from '../neon/sql';
+import { sqlApp } from '../neon/sql';
 
 export type Profile = {
   user_id: string;
@@ -8,47 +8,6 @@ export type Profile = {
   created_at: string;
   updated_at: string;
 };
-
-/*
-export async function withUserContext<T>(
-  userId: string,
-  queryFn: (tx: any) => any
-): Promise<T> {
-
-  const results = await sqlApp.transaction((tx) => [
-    tx`SELECT set_config('app.user_id', ${userId}, true)`,
-    queryFn(tx),
-  ])
-  //console.log("userId: ", userId);
-  return results[1] as T
-}
-*/
-
-/*
-export async function getMyProfile(
-  userId: string
-): Promise<Profile | null> {
-  return withUserContext<Profile | null>(userId, async (tx) => {
-    const rows = await tx`
-      select
-        user_id,
-        role,
-        avatar_url,
-        created_at,
-        updated_at
-      from profiles
-      where user_id = ${userId}
-      limit 1
-    `;
-
-    return rows[0] ?? null;
-  });
-}
-*/
-
-
-
-
 
 export async function withUserContext<T>(
   userId: string,
