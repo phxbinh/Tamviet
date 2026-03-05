5606576c555536c185bb97ce3dea4a0d25f6f690
phxbinh <phxbinh@gmail.com>
2026-03-05 15:47:40 +0700

    Update page.tsx

--- src/app/(public)/welcomemot/page.tsx
+++ src/app/(public)/welcomemot/page.tsx
@@ -2,6 +2,14 @@ import { createSupabaseServerClient } from '@/lib/supabase/server';
 import { redirect } from 'next/navigation';
 import WelcomeClient from './WelcomeClientGpt1';
 
+import { getCurrentUser } from '@/lib/authActions/getUser';
+  
+
+
+
+
+
+
 export default async function WelcomePage() {
   const supabase = await createSupabaseServerClient();
 
@@ -9,7 +17,8 @@ export default async function WelcomePage() {
 
   // Nếu không có user, đá về login ngay lập tức ở tầng Server
   if (!user) redirect('/login');
+  const userCurrent = await getCurrentUser();
 
   // Truyền data user vào Client Component
-  return <WelcomeClient user={user} />;
+  return <WelcomeClient user={userCurrent} />;
 }
