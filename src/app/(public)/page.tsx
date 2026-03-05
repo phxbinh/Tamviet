1ca8fece1edcd05dbbc54c73944ed969117236fc
phxbinh <phxbinh@gmail.com>
2026-03-05 14:29:56 +0700

    Update page.tsx

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -5,8 +5,6 @@ import Link  from "next/link";
 export default function PublicPage() {
   return (
     <div className="relative flex flex-col gap-10 min-h-screen">
-      <Link href="/public">Public route</Link>
-      <Link href="/auth">Auth route</Link>
       <Hero />
       <Features />
     </div>
