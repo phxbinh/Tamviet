ec42c09de161b0a5ac59fed3c6e42052f57f29b3
phxbinh <phxbinh@gmail.com>
2026-03-05 14:04:46 +0700

    Update layout.tsx

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -11,7 +11,7 @@ export default async function AuthLayout({
   return (
     <>
       {children}
-      <div className="fixed bottom-6 right-6 z-50"><ThemeToggle />
+      <div className="fixed bottom-6 right-6 z-50"><ThemeToggle /></div>
       <Toast/>
     </>
   );
