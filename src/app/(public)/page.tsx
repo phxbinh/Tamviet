fb82d01e046eb13778f15f08f57da9ac70fc5cfe
phxbinh <phxbinh@gmail.com>
2026-03-05 14:26:31 +0700

    Update page.tsx

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,10 +1,12 @@
+// src/app/(public)/page.tsx
 import { Hero } from "@/components/Hero";
 import { Features } from "@/components/Features";
-
+import Link  from "next/link";
 export default function PublicPage() {
   return (
-    // Sử dụng Fragment hoặc div để không lặp lại thẻ <main> từ layout
-    <div className="relative flex flex-col gap-10">
+    <div className="relative flex flex-col gap-10 min-h-screen">
+      <Link href="/public">Public route</Link>
+      <Link href="/auth">Auth route</Link>
       <Hero />
       <Features />
     </div>
