2ddad730d586253ec7479ca68d3079336e582126
phxbinh <phxbinh@gmail.com>
2026-03-08 14:40:17 +0700

    Update page.tsx

--- src/app/(public)/markdownTests/page.tsx
+++ src/app/(public)/markdownTests/page.tsx
@@ -216,7 +216,7 @@ export default function MarkdownTest() {
           </article>
 
           {/* MỤC LỤC (SIDEBAR) */}
-          <aside className="sticky top-24 self-start">
+          <aside className="sticky top-mt-32 self-start">
             <div className="bg-card/50 backdrop-blur-sm p-2 rounded-2xl border border-border shadow-xl shadow-black/5">
               <TableOfContents htmlContent={htmlContent} />
             </div>
