a28d78140caac20c61a3ccaf129c8397b77d7468
phxbinh <phxbinh@gmail.com>
2026-03-09 12:08:02 +0700

    Update BlogPost.tsx

--- src/components/md/BlogPost.tsx
+++ src/components/md/BlogPost.tsx
@@ -1,17 +1,3 @@
-export default function BlogPost() {
-  const contentRef = useRef<HTMLDivElement>(null);
-
-  return (
-    <article>
-      <header><h1>Tiêu đề bài viết</h1></header>
-      
-      {/* Truyền ref bài viết vào đây */}
-      <TableOfContents htmlContent={html} contentRef={contentRef} />
-
-      {/* Vùng nội dung chính dùng để tính % cuộn */}
-      <div ref={contentRef} className="prose lg:prose-xl">
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-      </div>
-    </article>
-  );
+export function BlogPost() {
+  
 }
