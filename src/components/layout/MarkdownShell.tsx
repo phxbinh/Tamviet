8db22d522de585c47ddd7a735a81a5f8078853d9
phxbinh <phxbinh@gmail.com>
2026-03-09 15:35:04 +0700

    Create MarkdownShell.tsx

---
+++ src/components/layout/MarkdownShell.tsx
@@ -0,0 +1,26 @@
+// src/components/layout/MarkdownShell.tsx
+"use client";
+
+import { ThemeToggle } from "@/components/ThemeToggle";
+import { Toast } from "@/components/Toast";
+
+
+export default function PublicShell({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+        {children}
+      <Toast />
+      <div className="fixed bottom-6 right-6 z-50">
+        <ThemeToggle />
+      </div>
+    </>
+  );
+}
+
+
+
+
