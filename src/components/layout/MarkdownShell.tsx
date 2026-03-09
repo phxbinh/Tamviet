9a4aad38016756f57ec751f31e2015f6326aa6bd
phxbinh <phxbinh@gmail.com>
2026-03-09 16:17:20 +0700

    Update MarkdownShell.tsx

--- src/components/layout/MarkdownShell.tsx
+++ src/components/layout/MarkdownShell.tsx
@@ -74,10 +74,13 @@ const [isOpen, setIsOpen] = useState(false);
             </div>
           </header>
         </div>
+
+<>        {children} </>
+
       </div>
 
 
-        {children}
+
       <Toast />
       <div className="fixed bottom-6 right-6 z-50">
         <ThemeToggle />
