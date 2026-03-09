e9c992b37c7a75e9566c79ff0f92d5dfd33920ec
phxbinh <phxbinh@gmail.com>
2026-03-09 12:16:01 +0700

    Update page.tsx

--- src/app/mdtest/page.tsx
+++ src/app/mdtest/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect, useRef } from 'react';
 import { parseMarkdown } from '@/lib/md/markdown';
-import TableOfContents from '@/components/md/TableOfContentsMD';
+import TableOfContents from '@/components/md/TableOfContentsMDF';
 
 // Giả định MOCK_MARKDOWN đã được import hoặc định nghĩa bên trên
 const MOCK_MARKDOWN = `
