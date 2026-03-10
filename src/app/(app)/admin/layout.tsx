5efc203d345bb45d7004881ca8797056b47b8855
phxbinh <phxbinh@gmail.com>
2026-03-10 13:03:02 +0700

    Create layout.tsx

---
+++ src/app/(app)/admin/layout.tsx
@@ -0,0 +1,29 @@
+
+// src/app/(auth)/dashboard/layout.tsx
+// làm việc trên tài khoản của user
+
+import { getCurrentUser } from '@/lib/authActions/getUser';
+import DashboardShell from '@/components/dashboard/DashboardShell';
+
+export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const user = await getCurrentUser();
+
+  // Ở đây không truyền hàm, chỉ truyền dữ liệu (Data)
+  return (
+    <DashboardShell user={user}>
+      {children}
+    </DashboardShell>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
