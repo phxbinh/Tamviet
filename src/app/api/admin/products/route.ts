b11c951fd73d4e18f74c0b6a8d897cc5da90c88c
phxbinh <phxbinh@gmail.com>
2026-03-11 21:01:12 +0700

    Update route.ts

--- src/app/api/admin/products/route.ts
+++ src/app/api/admin/products/route.ts
@@ -80,34 +80,6 @@ export async function POST(req: Request) {
   }
 }
 
-/*
-export async function GET() {
-  try {
-    //await assertAdmin()
-
-    const result = await sql`
-      select
-        id,
-        name,
-        slug,
-        status,
-        product_type,
-        created_at
-      from products
-      order by created_at desc
-    `
-
-    return NextResponse.json(result)
-  } catch (err) {
-    console.error(err)
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    )
-  }
-}
-*/
-
 export async function GET() {
   try {
     const result = await sql`
