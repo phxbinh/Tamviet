231f074ab0bc5bd07caa5b95c0f61e83b44749da
phxbinh <phxbinh@gmail.com>
2026-03-13 02:56:29 +0700

    Update route.ts

--- src/app/api/products/images/[id]/route.ts
+++ src/app/api/products/images/[id]/route.ts
@@ -1,6 +1,8 @@
 // src/app/api/products/images/route.ts
 import { sql } from "@/lib/neon/sql"
+import { supabase } from '@/lib/supabase/clientSupabase';
 
+/*
 export async function DELETE(
   req: Request,
   context: { params: Promise<{ id: string }> }
@@ -8,6 +10,47 @@ export async function DELETE(
 
   const { id } = await context.params
 
+  await sql`
+    delete from product_images
+    where id = ${id}
+  `
+
+  return Response.json({ ok: true })
+}
+*/
+
+
+
+
+export async function DELETE(
+  req: Request,
+  context: { params: Promise<{ id: string }> }
+) {
+  const { id } = await context.params
+
+  // 1️⃣ lấy path ảnh
+  const images = await sql`
+    select image_path
+    from product_images
+    where id = ${id}
+  `
+
+  if (images.length === 0) {
+    return Response.json({ error: "Image not found" }, { status: 404 })
+  }
+
+  const imagePath = images[0].image_path
+
+  // 2️⃣ xoá file trong storage
+  const { error } = await supabase.storage
+    .from("products-images")
+    .remove([imagePath])
+
+  if (error) {
+    return Response.json({ error: error.message }, { status: 500 })
+  }
+
+  // 3️⃣ xoá record DB
   await sql`
     delete from product_images
     where id = ${id}
