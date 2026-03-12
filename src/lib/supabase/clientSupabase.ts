1f95fb6fee2626847e41a9c783cee0534fbaa483
phxbinh <phxbinh@gmail.com>
2026-03-12 14:53:31 +0700

    Update clientSupabase.ts

--- src/lib/supabase/clientSupabase.ts
+++ src/lib/supabase/clientSupabase.ts
@@ -2,7 +2,15 @@
 
 import { createBrowserClient } from '@supabase/ssr';
 
+/*
 export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
+*/
+
+
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
