b342e99226c65a166177271290bfe3e42930a4eb
phxbinh <phxbinh@gmail.com>
2026-03-14 17:25:43 +0700

    Update page.tsx

--- src/app/(public)/testCategories/[[...slug]]/page.tsx
+++ src/app/(public)/testCategories/[[...slug]]/page.tsx
@@ -216,7 +216,7 @@ async function getProducts(path?: string) {
   const host = (await headers()).get("host");
   const url = path
     ? `http://${host}/api/products/categories?category=${path}`
-    : `http://${host}/api/products`;
+    : `http://${host}/api/products/categories`;
 //: `http://${host}/api/products/categories`
   const res = await fetch(url, { cache: "no-store" });
   if (!res.ok) return null;
