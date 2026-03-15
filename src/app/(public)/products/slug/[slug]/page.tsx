cf619036e67aa3ce70294593e3eaf2837e6d7213
phxbinh <phxbinh@gmail.com>
2026-03-15 20:20:24 +0700

    Update page.tsx

--- src/app/(public)/products/slug/[slug]/page.tsx
+++ src/app/(public)/products/slug/[slug]/page.tsx
@@ -1,32 +1,16 @@
-neimport ProductDetailClient from "@/features/products/components/ProductDetailClient";
+import ProductDetailClient from "@/features/products/components/ProductDetailClient";
 import { headers } from "next/headers";
-import { getProductFull } from "@/lib/server/products/getProductFull";
+import { getProductDetail_slug } from "@/lib/server/products/getProductDetail_slug";
 import { notFound } from "next/navigation";
-
-async function getProduct(id: string) {
-  const host = (await headers()).get("host");
-
-  const res = await fetch(
-    `http://${host}/api/admin/products/${id}/full`,
-    { cache: "no-store" }
-  );
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch product");
-  }
-
-  return res.json();
-}
 
 export default async function ProductPage({
   params,
 }: {
-  params: Promise<{ id: string }>;
+  params: Promise<{ slug: string }>;
 }) {
-  const { id } = await params;
+  const { slug } = await params;
 
-  //const data = await getProduct(id);
-  const data = await getProductFull(id);
+  const data = await getProductDetail_slug(slug);
 
 if (!data) {
     notFound();
