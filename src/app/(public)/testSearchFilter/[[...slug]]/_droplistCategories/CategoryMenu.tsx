d4648998987906c5f3394c50e675abd55514fccb
phxbinh <phxbinh@gmail.com>
2026-03-20 20:53:01 +0700

    Create CategoryMenu.tsx

---
+++ src/app/(public)/testSearchFilter/[[...slug]]/_droplistCategories/CategoryMenu.tsx
@@ -0,0 +1,14 @@
+import { getCategoriesTree } from '@/lib/categories';
+import { CategoryItem } from './CategoryItem';
+
+export default async function CategoriesMenu({ path }: { path: string }) {
+  const categories = await getCategoriesTree();
+
+  return (
+    <div className="flex gap-2">
+      {categories.map((cat) => (
+        <CategoryItem key={cat.id} cat={cat} path={path} />
+      ))}
+    </div>
+  );
+}
