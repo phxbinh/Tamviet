97a7fa1946f1619434f75ae0a71778452a5b155d
phxbinh <phxbinh@gmail.com>
2026-03-22 00:56:46 +0700

    Update AppShell.tsx

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import { Toast } from "@/components/Toast";
 import { ThemeToggle } from "@/components/ThemeToggle";
 */
-/*
+
 export default function AppShell({
   children,
 }: {
@@ -18,29 +18,30 @@ export default function AppShell({
     </>
   );
 }
-*/
 
+
+/*
 export default function AppShell({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* HEADER */}
+ 
       <header className="sticky top-0 z-50">
         
       </header>
 
-      {/* MAIN */}
+
       <main className="flex-1">
         {children}
       </main>
 
-      {/* FOOTER */}
+
       <footer>
         
       </footer>
     </div>
   );
 }
-
+*/
 
 
 
