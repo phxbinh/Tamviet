ccee08d6bd21e8d96703e4681c7b3c3b192299c8
phxbinh <phxbinh@gmail.com>
2026-03-21 13:13:39 +0700

    Update AppShell.tsx

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { useState } from "react";
+/*
 import Sidebar from "@/components/sidebar/Sidebar";
 import { Menu, X } from "lucide-react";
 import Link from "next/link";
 import { Toast } from "@/components/Toast";
 import { ThemeToggle } from "@/components/ThemeToggle";
-
+*/
 export default function AppShell({
   children,
 }: {
