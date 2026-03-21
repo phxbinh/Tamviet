321641f3e1c732ebd324cb574d26fdc4f4141937
phxbinh <phxbinh@gmail.com>
2026-03-22 00:21:16 +0700

    Update AppShell.tsx

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,6 +1,4 @@
-"use client";
 
-import { useState } from "react";
 /*
 import Sidebar from "@/components/sidebar/Sidebar";
 import { Menu, X } from "lucide-react";
@@ -8,17 +6,42 @@ import Link from "next/link";
 import { Toast } from "@/components/Toast";
 import { ThemeToggle } from "@/components/ThemeToggle";
 */
+/*
 export default function AppShell({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
     <>
-      {/* toàn bộ UI bạn viết chuyển vào đây */}
       {children}
     </>
   );
 }
+*/
+
+export default function AppShell({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      {/* HEADER */}
+      <header className="sticky top-0 z-50">
+        
+      </header>
+
+      {/* MAIN */}
+      <main className="flex-1">
+        {children}
+      </main>
+
+      {/* FOOTER */}
+      <footer>
+        
+      </footer>
+    </div>
+  );
+}
+
+
+
+
+
