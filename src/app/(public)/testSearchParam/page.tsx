b293684db51660ffa2019db107b3ac2f9204984f
phxbinh <phxbinh@gmail.com>
2026-03-22 16:08:47 +0700

    Update page.tsx

--- src/app/(public)/testSearchParam/page.tsx
+++ src/app/(public)/testSearchParam/page.tsx
@@ -85,7 +85,7 @@ export default async function Page({
             productsLength={products.length}
           />
         </StickyFilterWrapper>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 mt-2">
         {/* 3. PRODUCT GRID: Sử dụng Component Số 1 */}
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-1 gap-y-1 md:gap-x-4 md:gap-y-4">
           {products.length === 0 ? (
