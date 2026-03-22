52d1a55ae03ea40db3a36b27a057aeda3617f2bb
phxbinh <phxbinh@gmail.com>
2026-03-22 12:40:08 +0700

    Update page.tsx

--- src/app/(public)/testSearchParam/page.tsx
+++ src/app/(public)/testSearchParam/page.tsx
@@ -86,6 +86,7 @@ export default async function Page({
             path={path}
             productsLength={products.length}
         />
+</div> <div>
 
         {/* 3. PRODUCT GRID: Sử dụng Component Số 1 */}
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-1 gap-y-1 md:gap-x-4 md:gap-y-4">
