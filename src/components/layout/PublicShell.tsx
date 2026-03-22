a1d158ca85afb1cb1ee43d45953f208888a823a1
phxbinh <phxbinh@gmail.com>
2026-03-22 17:08:33 +0700

    Update PublicShell.tsx

--- src/components/layout/PublicShell.tsx
+++ src/components/layout/PublicShell.tsx
@@ -31,7 +31,7 @@ export default function PublicShell({
 
   return (
     <>
-      <div className="flex min-h-screen bg-background no-scrollbar">
+      <div className="flex min-h-screen bg-background">
 
         {/* SIDEBAR */}
         <aside
@@ -69,7 +69,7 @@ export default function PublicShell({
         )}
 
         {/* MAIN */}
-        <div className="flex-1 flex flex-col min-w-0 no-scrollbar">
+        <div className="flex-1 flex flex-col min-w-0">
 
           {/* HEADER */}
           <header className="sticky top-0 z-30 h-16 flex items-center px-4 border-b border-border bg-background/70 backdrop-blur-md">
@@ -82,7 +82,7 @@ export default function PublicShell({
           </header>
 
           {/* ✅ SCROLL CONTAINER - KHÔNG WRAPPER */}
-          <main className="flex-1 px-1 md:px-8 pb-4 pt-0 overflow-y-auto no-scrollbar">
+          <main className="flex-1 px-1 md:px-8 pb-4 pt-0">
             {children}
           </main>
 
