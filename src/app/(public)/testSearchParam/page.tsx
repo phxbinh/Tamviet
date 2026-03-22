e5024863059b4ff0eaa0f5974103570eb4b384fe
phxbinh <phxbinh@gmail.com>
2026-03-22 22:28:21 +0700

    Update page.tsx

--- src/app/(public)/testSearchParam/page.tsx
+++ src/app/(public)/testSearchParam/page.tsx
@@ -41,7 +41,7 @@ export default async function Page({
     ? (rawSort as SortType)
     : "newest";
 
-  const limit = 8;
+  const limit = 3;
 
   const [products, categories, productTypesData] = await Promise.all([
     getProductsByCategory({ 
