de3a18a1754a937b8f9584c71399f435fc0ad697
phxbinh <phxbinh@gmail.com>
2026-03-22 15:07:06 +0700

    Update useScrollDirection.ts

--- src/app/(public)/testSearchParam/_scrollsticky/useScrollDirection.ts
+++ src/app/(public)/testSearchParam/_scrollsticky/useScrollDirection.ts
@@ -142,7 +142,7 @@ export function useScrollDirection() {
 }
 */
 
-
+/*
 'use client'
 
 import { useEffect, useRef, useState } from "react";
@@ -200,6 +200,51 @@ export function useScrollDirection() {
 
   return direction;
 }
+*/
 
+
+'use client'
+
+import { useEffect, useRef, useState } from "react";
+
+export function useScrollDirection() {
+  const [direction, setDirection] = useState<"up" | "down">("up");
+
+  const lastScroll = useRef(0);
+  const accumulated = useRef(0);
+
+  const THRESHOLD = 80;
+
+  useEffect(() => {
+    lastScroll.current = window.scrollY;
+
+    const handleScroll = () => {
+      const current = window.scrollY;
+
+      const delta = current - lastScroll.current;
+
+      if (Math.abs(delta) < 2) return;
+
+      accumulated.current += delta;
+
+      if (accumulated.current > THRESHOLD) {
+        setDirection((prev) => (prev !== "down" ? "down" : prev));
+        accumulated.current = 0;
+      }
+
+      if (accumulated.current < -THRESHOLD) {
+        setDirection((prev) => (prev !== "up" ? "up" : prev));
+        accumulated.current = 0;
+      }
+
+      lastScroll.current = current;
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return direction;
+}
 
 
