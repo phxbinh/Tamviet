0c12a828476af7ec2888252bd10d3d1e322b3469
phxbinh <phxbinh@gmail.com>
2026-03-23 20:20:27 +0700

    Create SearchUI.tsx

---
+++ src/app/(public)/testSearchParam/_tachComponents/SearchUI.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useState } from 'react';
+import { FilterTrigger } from './FilterTrigger';
+import { FilterPanel } from './FilterPanel';
+import { StickyFilterWrapper } from '../_scrollsticky/StickyFilterWrapper';
+
+interface Props {
+  productTypes: any[];
+  categoryTree: any[];
+  path: string;
+}
+
+export function SearchUI({
+  productTypes,
+  categoryTree,
+  path
+}: Props) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <>
+      {/* 🔥 Sticky */}
+      <StickyFilterWrapper>
+        <FilterTrigger
+          isExpanded={isExpanded}
+          setIsExpanded={setIsExpanded}
+        />
+      </StickyFilterWrapper>
+
+      {/* 🔥 Panel */}
+      <FilterPanel
+        isExpanded={isExpanded}
+        setIsExpanded={setIsExpanded}
+        productTypes={productTypes}
+        categoryTree={categoryTree}
+        path={path}
+      />
+    </>
+  );
+}
