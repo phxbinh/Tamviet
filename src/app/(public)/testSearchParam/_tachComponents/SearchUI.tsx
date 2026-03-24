326e579669ac4138aa770369a2dc5d1598589040
phxbinh <phxbinh@gmail.com>
2026-03-24 10:54:02 +0700

    Update SearchUI.tsx

--- src/app/(public)/testSearchParam/_tachComponents/SearchUI.tsx
+++ src/app/(public)/testSearchParam/_tachComponents/SearchUI.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { FilterTrigger } from './FilterTrigger';
-import { FilterPanel } from './FilterPanel_';
+import { FilterPanel } from './FilterPanel';
 import { StickyFilterWrapper } from '../_scrollsticky/StickyFilterWrapper';
 
 interface Props {
