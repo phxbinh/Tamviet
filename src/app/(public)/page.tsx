e31f1fd2c0ca0ae2e34feda22b37d0df0b2da321
phxbinh <phxbinh@gmail.com>
2026-03-24 13:53:33 +0700

    Update page.tsx

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,4 +1,5 @@
 // src/app/(public)/page.tsx
+/*
 import { Hero } from "@/components/Hero";
 import { Features } from "@/components/Features";
 import Link  from "next/link";
@@ -7,6 +8,42 @@ export default function PublicPage() {
     <div className="relative flex flex-col gap-10 min-h-screen">
       <Hero />
       <Features />
+    </div>
+  );
+}
+*/
+
+
+// app/page.tsx
+
+import RelatedProductsSection from "./_homepage/RelatedProductsSection";
+import { getProductsByType } from "./_homepage/getProductsByType";
+import { getProductTypes } from "./_homepage/getProductTypes";
+
+export default async function HomePage() {
+
+  const productTypes = await getProductTypes();
+
+  // 🔥 fetch song song
+  const sections = await Promise.all(
+    productTypes.map(async (type: any) => {
+      const products = await getProductsByType(type.code, 8);
+      return {
+        type,
+        products
+      };
+    })
+  );
+
+  return (
+    <div className="space-y-16">
+      {sections.map(({ type, products }) => (
+        <RelatedProductsSection
+          key={type.code}
+          relatedProducts={products}
+          title={type.name} // 👈 thêm title
+        />
+      ))}
     </div>
   );
 }
