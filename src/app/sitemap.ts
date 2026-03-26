e13d30e282e198497a07ecb6838c3c70c28e09c6
phxbinh <phxbinh@gmail.com>
2026-03-26 13:52:59 +0700

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://tamviet.vercel.app' // Thay bằng domain của bạn
 
   // Giả sử bạn có danh sách bài viết Markdown
-  const routes = ['', '/test', '/testSearchParam', '/products'].map((route) => ({
+  const routes = ['', '/test', '/testSearchParam', '/roadmap'].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date(),
     changeFrequency: 'daily' as const,
