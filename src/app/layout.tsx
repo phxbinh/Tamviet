8127d9b53b09c38846be63fd6b9bbac329c0d705
phxbinh <phxbinh@gmail.com>
2026-03-26 21:58:58 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,13 +54,16 @@ export const metadata: Metadata = {
     type: 'website',
   },
   icons: {
-    icon: '/favicon.ico', // Icon nhỏ trên tab trình duyệt
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon.ico" }, 
+    ],
     apple: [
-      { url: "/apple-icon.png", sizes: "180x180", type: "image/png" },
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
   },
 };
-
 
 const inter = Inter({ subsets: ["latin"] });
 /*
