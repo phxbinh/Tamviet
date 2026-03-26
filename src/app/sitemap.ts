6b6ff440d3a428ec686f5bc127e900cccb887542
phxbinh <phxbinh@gmail.com>
2026-03-26 13:52:30 +0700

    Create sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,15 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://tamviet.vercel.app' // Thay bằng domain của bạn
+
+  // Giả sử bạn có danh sách bài viết Markdown
+  const routes = ['', '/test', '/testSearchParam', '/products'].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date(),
+    changeFrequency: 'daily' as const,
+    priority: 1,
+  }))
+
+  return [...routes]
+}
