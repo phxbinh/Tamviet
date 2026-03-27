e90a6cc87d94641ccb56e32704e456eae0a18fcc
phxbinh <phxbinh@gmail.com>
2026-03-28 00:53:20 +0700

    Update PublicShell_.tsx

--- src/components/layout/PublicShell_.tsx
+++ src/components/layout/PublicShell_.tsx
@@ -82,7 +82,7 @@ export default function PublicShell({
           </header>
 
           {/* ✅ SCROLL CONTAINER - KHÔNG WRAPPER */}
-          <main className="flex-1 px-1 md:px-8 pb-4 pt-0">
+          <main className="flex-1 px-0 md:px-4 pb-4 pt-0">
             {children}
           </main>
 
