4126b7b1cb37851b755dac629f81107959ee0bb6
phxbinh <phxbinh@gmail.com>
2026-03-28 12:48:25 +0700

    Update layout.tsx

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,6 +1,6 @@
 // src/app/(public)/layout.tsx
 // src/app/(public)/layout.tsx
-import PublicShell from "@/components/layout/PublicShell_x";
+import PublicShell from "@/components/layout/PublicShell";
 
 export default function PublicLayout({
   children,
