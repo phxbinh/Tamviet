2ae8d99fa1465a10b92d6d0b1799605eaac71c78
phxbinh <phxbinh@gmail.com>
2026-03-28 20:41:46 +0700

    Update ProductDetailClient.tsx

--- src/app/(public)/testSearchParam/products/[slug]/_components/ProductDetailClient.tsx
+++ src/app/(public)/testSearchParam/products/[slug]/_components/ProductDetailClient.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { useState, useMemo } from "react";
 import { ProductFull } from "../types";
-import { ProductGallery } from "./ProductGallery_x";
-import { ProductInfo } from "./ProductInfo_x";
+import { ProductGallery } from "./ProductGallery";
+import { ProductInfo } from "./ProductInfo";
 
 export default function ProductDetailClient({ data }: { data: ProductFull }) {
   const { product, attributes, variants, images } = data;
