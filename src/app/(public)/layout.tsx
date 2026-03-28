9e0e96f994b494870b861fe87537c21bb330d5e2
phxbinh <phxbinh@gmail.com>
2026-03-28 13:15:53 +0700

    Update layout.tsx

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -2,10 +2,24 @@
 // src/app/(public)/layout.tsx
 import PublicShell from "@/components/layout/PublicShell";
 
+/*
 export default function PublicLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return <PublicShell>{children}</PublicShell>;
+}
+*/
+
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+ 
+      </div>
+      <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
 }
