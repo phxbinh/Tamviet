a08096a2e96230364f981d750118e8919ec28eee
phxbinh <phxbinh@gmail.com>
2026-03-29 15:41:55 +0700

    Update AdminSidebar.tsx

--- src/components/admin/AdminSidebar.tsx
+++ src/components/admin/AdminSidebar.tsx
@@ -63,14 +63,15 @@ function AdminSidebar_({ user, onNavigate }: AdminSidebarProps) {
   ];
 
   return (
-    /*<aside className="flex flex-col h-full bg-card/50 backdrop-blur-xl border-r border-border w-64 transition-all duration-300">*/
-      <aside className={`
+/*      <aside className={`
           fixed inset-y-0 left-0 z-50 w-64 
           bg-card/80 backdrop-blur-2xl border-r border-border
           transition-transform duration-300 ease-in-out
           lg:relative lg:translate-x-0
           ${isOpen ? "translate-x-0" : "-translate-x-full"}
-        `}>
+        `}>*/
+    <aside className="flex flex-col h-full bg-card/50 backdrop-blur-xl border-r border-border w-64 transition-all duration-300">
+ 
       {/* LOGO */}
       <div className="h-16 flex items-center px-6 border-b border-border shrink-0">
         <Link href="/dashboard" className="flex items-center gap-2 group">
