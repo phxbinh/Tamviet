773c94e943a0f1199aaa86dfb9a348d15956f9dd
phxbinh <phxbinh@gmail.com>
2026-03-30 13:24:59 +0700

    Update Sidebar.tsx

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,17 +1,13 @@
 // src/components/sidebar/Sidebar.tsx
 import SidebarClient from './SidebarClient';
 import { sidebarLinks } from './links';
-import {ThemeToggle} from '@/components/ThemeToggle_lr';
 
 export default function Sidebar({ onNavigate }: { onNavigate?: () => void }) {
   const visibleLinks = sidebarLinks.filter((l) => l.showInSidebar !== false);
 
   return (
-<>
     <div className="py-4 z-50">
       <SidebarClient links={visibleLinks} onNavigate={onNavigate} />
     </div>
-    <ThemeToggle/>
-</>
   );
 }
