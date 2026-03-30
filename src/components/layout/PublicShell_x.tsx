1bea17aa012d82b1df22cce1ad47ceaf6faf9f71
phxbinh <phxbinh@gmail.com>
2026-03-30 13:26:02 +0700

    Update PublicShell_x.tsx

--- src/components/layout/PublicShell_x.tsx
+++ src/components/layout/PublicShell_x.tsx
@@ -3,7 +3,7 @@
 
 import { useState } from "react";
 import Sidebar from "@/components/sidebar/Sidebar";
-import { ThemeToggle } from "@/components/ThemeToggle_lr";
+import { ThemeToggle } from "@/components/ThemeToggle_";
 import { Toast } from "@/components/Toast";
 import { Menu, X, ShoppingCart } from "lucide-react";
 import Link from "next/link";
