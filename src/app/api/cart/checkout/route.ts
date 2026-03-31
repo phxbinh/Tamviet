d1f3ac8d0b9d087dd2e4f3922449bcc296cb0ec3
phxbinh <phxbinh@gmail.com>
2026-04-01 02:25:27 +0700

    Create route.ts

---
+++ src/app/api/cart/checkout/route.ts
@@ -0,0 +1,15 @@
+// app/api/checkout/route.ts
+import { checkout } from "@/lib/checkout/checkout";
+import { getCurrentUser } from "@/lib/authActions/getUser";
+
+export async function POST() {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    return Response.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const result = await checkout({ userId: user.id });
+
+  return Response.json(result);
+}
