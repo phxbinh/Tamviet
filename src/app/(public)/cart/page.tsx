ba3a680b809eed57b7f14cbefcf3df214fa3b99c
phxbinh <phxbinh@gmail.com>
2026-04-02 16:27:29 +0700

    Update page.tsx

--- src/app/(public)/cart/page.tsx
+++ src/app/(public)/cart/page.tsx
@@ -22,7 +22,7 @@ import CheckoutForm from "@/components/checkout/CheckoutForm";
 
 
 //export default 
-function CartPage//() {
+function CartPage__() {
   const [isCheckingOut, setIsCheckingOut] = useState(false);
   const { cart, setCart, fetchCart, loading } = useCart();
 
