f3242f23cb276d2f4015e3d0a09bbe23879ac562
phxbinh <phxbinh@gmail.com>
2026-04-03 20:02:18 +0700

    Create useClientInfo_.ts

---
+++ src/app/(public)/userInfo/useClientInfo_.ts
@@ -0,0 +1,27 @@
+'use server'
+import { headers } from 'next/headers';
+
+export async function getClientData() {
+  const headerList = await headers();
+  
+  // Lấy IP: Vercel trả về chuỗi IP, đôi khi kèm port hoặc nhiều IP (proxy), ta lấy cái đầu tiên
+  const forwarded = headerList.get('x-forwarded-for');
+  const ip = forwarded ? forwarded.split(',')[0] : 'Unknown';
+
+  // Lấy vị trí từ Vercel Edge
+  const rawCity = headerList.get('x-vercel-ip-city') || 'Ho Chi Minh City';
+  const country = headerList.get('x-vercel-ip-country') || 'VN';
+  
+  // Fix lỗi %20 ngay tại đây để Client nhận data sạch
+  const city = decodeURIComponent(rawCity).replace(/\+/g, ' ');
+
+  // Thông tin thiết bị
+  const ua = headerList.get('user-agent') || 'Unknown Device';
+
+  return { 
+    ip, 
+    city, 
+    country, 
+    ua 
+  };
+}
