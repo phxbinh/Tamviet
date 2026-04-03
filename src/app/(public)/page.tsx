6fc3cf6f70c79fdd0a605a31f9fa9d25784223ed
phxbinh <phxbinh@gmail.com>
2026-04-03 14:04:50 +0700

    Update page.tsx

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,68 +1,4 @@
 // src/app/(public)/page.tsx
-/*
-import { Hero } from "@/components/Hero";
-import { Features } from "@/components/Features";
-import Link  from "next/link";
-export default function PublicPage() {
-  return (
-    <div className="relative flex flex-col gap-10 min-h-screen">
-      <Hero />
-      <Features />
-    </div>
-  );
-}
-*/
-
-
-// app/page.tsx
-/*
-import RelatedProductsSection from "./_homepage/RelatedProductsSection";
-import { getProductsByType } from "./_homepage/getProductsByType";
-import { getProductTypes } from "./_homepage/getProductTypes";
-
-export default async function HomePage() {
-
-  const productTypes = await getProductTypes();
-const sections = [
-  {
-    products: await getProductsByType("pumps", 8)
-  }
-];
-
-  return (
-    <div className="space-y-16">
-      {sections.map(({ products }) => (
-        <RelatedProductsSection
-          relatedProducts={products}
-        />
-      ))}
-    </div>
-  );
-}
-*/
-  // 🔥 fetch song song
-/* Không chạy được
-  const sections = await Promise.all(
-    productTypes.map(async (type: any) => {
-      const products = await getProductsByType(type.code, 8);
-      return {
-        type,
-        products
-      };
-    })
-  );*/
-
-
-
-
-
-
-
-
-
-
-
-// app/page.tsx (Server Component)
 
 // Thêm 2 dòng này lên trên cùng file
 export const revalidate = 300; 
@@ -112,21 +48,6 @@ return (
 
 }
 
-/*
-    <div className="max-w-3xl mx-auto p-10">
-      <StoreMap 
-        shopName="Tâm Việt Studio"
-        address="123 Đường ABC, Quận 1, TP. Hồ Chí Minh"
-        lat={10.845694} 
-        lng={106.656222} 
-      />
-    </div>
-
-
-10.845694, 106.656222
-
-
-*/
 
 
 
