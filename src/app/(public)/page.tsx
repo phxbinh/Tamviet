50e7be7a65fb798d08ad401ef6fba3d2d3b045e2
phxbinh <phxbinh@gmail.com>
2026-04-03 15:00:16 +0700

    Update page.tsx

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -6,9 +6,6 @@ export const revalidate = 300;
 import { getHomeSections, getHomeSections_array } from "./_homepage/getHomeSections"; // Đường dẫn file chứa hàm SQL mới
 import RelatedProductsSection from "./_homepage/RelatedProductsSectionO__";
 import StoreMap from './_map/StoreMap';
-import MapPreview from './_map/MapPreview';
-
-
 
 export default async function HomePage() {
   // Lấy toàn bộ data (gồm tên loại và danh sách sản phẩm của loại đó)
@@ -36,12 +33,6 @@ return (
   </div>
   <div className="max-w-3xl mx-auto p-1">
       <StoreMap 
-        shopName="Tâm Việt"
-        lat={10.845694} 
-        lng={106.656222} 
-        address="319/22 Đ. Lê Văn Thọ, Phường 9, Thông Tây Hội, Hồ Chí Minh"
-      />
-      <MapPreview 
         shopName="Tâm Việt"
         lat={10.845694} 
         lng={106.656222} 
