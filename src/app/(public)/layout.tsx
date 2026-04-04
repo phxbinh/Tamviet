bbb89f5018f3651c09ac8177a76e24d40488896d
phxbinh <phxbinh@gmail.com>
2026-04-04 15:19:45 +0700

    Update layout.tsx

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,4 +1,5 @@
 // src/app/(public)/layout.tsx
+/*
 import PublicShell from "@/components/layout/PublicShell_x";
 
 export default function PublicLayout({
@@ -8,7 +9,20 @@ export default function PublicLayout({
 }) {
   return <PublicShell>{children}</PublicShell>;
 }
+*/
 
+
+import { getCurrentUser } from '@/lib/authActions/getUser';
+import PublicShell from "@/components/layout/PublicShell_x_client";
+
+export default function PublicLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = await getCurrentUser();
+  return <PublicShell user={user}>{children}</PublicShell>;
+}
 
 
 
