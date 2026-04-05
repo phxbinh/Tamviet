c04e5adbaeaf3dfdb786667a64e2dc5273113627
phxbinh <phxbinh@gmail.com>
2026-04-05 21:24:59 +0700

    Update index.ts

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,6 +1,6 @@
 // lib/db/index.ts
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { pool } from './pg'; // File pg.ts của bạn
-import * as schema from '@/db/schema';
+import * as schema from './schemaEditor';
 
 export const db = drizzle(pool, { schema });
