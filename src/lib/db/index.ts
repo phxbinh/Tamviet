3934c4bd620e96fbb7deb248f6bce98df157737c
phxbinh <phxbinh@gmail.com>
2026-04-07 11:57:36 +0700

    Delete index.ts

--- src/lib/db/index.ts
+++
@@ -1,6 +0,0 @@
-// lib/db/index.ts
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { pool } from './pg'; // File pg.ts của bạn
-import * as schema from './schemaEditor';
-
-export const db = drizzle(pool, { schema });
