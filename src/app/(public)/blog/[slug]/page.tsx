cb1e95abfabf23e1ed7fbc7f1c0132578c97ba8d
phxbinh <phxbinh@gmail.com>
2026-04-08 01:49:20 +0700

    Update page.tsx

--- src/app/(public)/blog/[slug]/page.tsx
+++ src/app/(public)/blog/[slug]/page.tsx
@@ -25,6 +25,7 @@ export default async function Page({
 */
 
 
+/*
 import { sql } from "@/lib/neon/sql";
 import { Renderer } from "@/components/editor/Renderer";
 import { parseContent } from "@/lib/parseContent";
@@ -46,17 +47,64 @@ export default async function Page({
 
   return (
     <div className="max-w-3xl mx-auto px-6 py-10">
-      {/* TITLE */}
+
       <h1 className="text-4xl font-bold mb-8 leading-tight">
         {post[0].title}
       </h1>
+
+      <Renderer content={content} />
+    </div>
+  );
+}
+*/
+
+import { sql } from "@/lib/neon/sql";
+import { Renderer } from "@/components/editor/Renderer";
+import { parseContent } from "@/lib/parseContent";
+import { CalendarDays } from "lucide-react";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+
+  const post = await sql`
+    SELECT * FROM posts WHERE slug = ${slug}
+  `;
+
+  if (!post[0]) return <div className="p-6">Not found</div>;
+
+  const content = parseContent(post[0].content_json);
+
+  const formattedDate = new Date(post[0].created_at).toLocaleDateString(
+    "vi-VN",
+    {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }
+  );
+
+  return (
+    <div className="max-w-3xl mx-auto px-6 py-10">
+      {/* TITLE */}
+      <h1 className="text-4xl font-bold leading-tight">
+        {post[0].title}
+      </h1>
+
+      {/* META */}
+      <div className="flex items-center gap-2 text-gray-500 mt-3 mb-8 text-sm">
+        <CalendarDays className="w-4 h-4" />
+        <span>{formattedDate}</span>
+      </div>
 
       {/* CONTENT */}
       <Renderer content={content} />
     </div>
   );
 }
-
 
 
 
