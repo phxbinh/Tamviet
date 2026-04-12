d8ca11c5378cbb5f1e7e9fc1f6c858eb87ad0af6
phxbinh <phxbinh@gmail.com>
2026-04-12 15:56:16 +0700

    Update Renderer.tsx

--- src/app/(markdown)/baivietapp/_src/Renderer.tsx
+++ src/app/(markdown)/baivietapp/_src/Renderer.tsx
@@ -1,20 +1,24 @@
 'use client';
 
-import type { Document, Block } from "@/lib/blocks";
+import type { Document, Block } from "@/baivietapp/_src/blocks";
 
 /**
- * Render inline text (TextSchema)
+ * Render TextSchema
  */
-function renderText(node: any, i: number) {
+function renderText(
+  node: {
+    type: "text";
+    text: string;
+    bold?: boolean;
+    italic?: boolean;
+    href?: string;
+  },
+  i: number
+) {
   let el: React.ReactNode = node.text;
 
-  if (node.bold) {
-    el = <strong>{el}</strong>;
-  }
-
-  if (node.italic) {
-    el = <em>{el}</em>;
-  }
+  if (node.bold) el = <strong>{el}</strong>;
+  if (node.italic) el = <em>{el}</em>;
 
   if (node.href) {
     el = (
@@ -28,55 +32,6 @@ function renderText(node: any, i: number) {
 }
 
 /**
- * Render block
- */
-function renderBlock(block: Block, i: number) {
-  switch (block.type) {
-    case "heading": {
-      const Tag = `h${block.level}` as keyof JSX.IntrinsicElements;
-      return <Tag key={i}>{block.text}</Tag>;
-    }
-
-    case "paragraph":
-      return (
-        <p key={i}>
-          {block.content.map(renderText)}
-        </p>
-      );
-
-    case "list":
-      return (
-        <ul key={i}>
-          {block.items.map((item, idx) => (
-            <li key={idx}>
-              {item.map(renderText)}
-            </li>
-          ))}
-        </ul>
-      );
-
-    case "image":
-      return (
-        <img
-          key={i}
-          src={block.src}
-          alt={block.alt || ""}
-        />
-      );
-
-    case "code":
-      return (
-        <pre key={i}>
-          <code>{block.code}</code>
-        </pre>
-      );
-
-    default:
-      return null;
-  }
-}
-
-/**
  * MAIN Renderer
  */
 export default function Renderer({ content }: { content: Document }) {
@@ -84,7 +39,51 @@ export default function Renderer({ content }: { content: Document }) {
 
   return (
     <div>
-      {content.blocks.map(renderBlock)}
+      {content.blocks.map((block: Block, i: number) => {
+        switch (block.type) {
+          case "heading": {
+            const Tag = `h${block.level}` as keyof JSX.IntrinsicElements;
+            return <Tag key={i}>{block.text}</Tag>;
+          }
+
+          case "paragraph":
+            return (
+              <p key={i}>
+                {block.content.map(renderText)}
+              </p>
+            );
+
+          case "list":
+            return (
+              <ul key={i}>
+                {block.items.map((item, idx) => (
+                  <li key={idx}>
+                    {item.map(renderText)}
+                  </li>
+                ))}
+              </ul>
+            );
+
+          case "image":
+            return (
+              <img
+                key={i}
+                src={block.src}
+                alt={block.alt || ""}
+              />
+            );
+
+          case "code":
+            return (
+              <pre key={i}>
+                <code>{block.code}</code>
+              </pre>
+            );
+
+          default:
+            return null;
+        }
+      })}
     </div>
   );
 }
