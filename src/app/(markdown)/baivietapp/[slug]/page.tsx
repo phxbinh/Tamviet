cf4ec8db7d24cde571d0f258900eccced834e2a7
phxbinh <phxbinh@gmail.com>
2026-04-13 00:48:46 +0700

    Create page.tsx

---
+++ src/app/(markdown)/baivietapp/[slug]/page.tsx
@@ -0,0 +1,55 @@
+// src/app/(markdown)/baiviet/[slug]/page.tsx
+// Lỗi: phải deploy lại mới lấy được data mới .
+
+import { sql } from "@/lib/neon/sql";
+import { Renderer } from "@/lib/postLib/Renderer";
+import { parseContent } from "@/lib/postLib/parseContent";
+import { CalendarDays } from "lucide-react";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+
+  const post = await sql`
+    SELECT * FROM posts WHERE slug = ${slug}
+  `;
+
+  if (!post[0]) return <div className="p-6">Not found</div>;
+
+  const content = parseContent(post[0].content_json);
+
+  const formattedDate = new Date(post[0].created_at).toLocaleDateString(
+    "vi-VN",
+    {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }
+  );
+
+  return (
+    <div className="max-w-6xl mx-auto px-1 py-10">
+      {/* TITLE */}
+      <h1 className="text-4xl font-bold leading-tight">
+        {post[0].title}
+      </h1>
+
+      {/* META */}
+      <div className="flex items-center gap-2 text-gray-500 mt-3 mb-8 text-sm">
+        <CalendarDays className="w-4 h-4" />
+        <span>{formattedDate}</span>
+      </div>
+
+      {/* CONTENT */}
+      <pre className="bg-black text-green-400 p-4 rounded">
+        {JSON.stringify(content, null, 2)}</pre>
+      <Renderer content={content} />
+    </div>
+  );
+}
+
+
+
