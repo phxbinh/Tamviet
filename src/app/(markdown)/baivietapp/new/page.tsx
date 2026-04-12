d9879702bfbf409aa72c711d57710903fb7060bd
phxbinh <phxbinh@gmail.com>
2026-04-12 22:40:25 +0700

    Create page.tsx

---
+++ src/app/(markdown)/baivietapp/new/page.tsx
@@ -0,0 +1,11 @@
+// /baivietapp/new/page.tsx
+import Editor from "@/lib/postLib/Editor";
+
+export default function NewPostPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-bold mb-4">Tạo bài viết</h1>
+      <Editor />
+    </div>
+  );
+}
