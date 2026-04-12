46dbbddc716ff440fdd590c829d51f88e016b617
phxbinh <phxbinh@gmail.com>
2026-04-12 16:34:41 +0700

    Update page.tsx

--- src/app/(markdown)/baivietapp/review/[slug]/page.tsx
+++ src/app/(markdown)/baivietapp/review/[slug]/page.tsx
@@ -3,13 +3,10 @@ import { Renderer } from "../../_src/Renderer";
 import { Document } from "../../_src/blocks";
 import { notFound } from "next/navigation";
 
-type PostRow = {
-  id: string;
-  title: string;
-  content_json: Document;
-};
+import {CalendarDays} from "lucide-react";
 
 
+/*
 export default async function Page({
   params,
 }: {
@@ -31,3 +28,55 @@ export default async function Page({
     </div>
   );
 }
+*/
+
+
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+
+  const post = await sql`
+    SELECT * FROM posts WHERE slug = ${slug}
+  `;
+
+  if (!post[0]) return <div className="p-6">Not found</div>;
+
+  const content = parseContent(post[0].content_json);
+
+  const formattedDate = new Date(post[0].created_at).toLocaleDateString(
+    "vi-VN",
+    {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }
+  );
+
+  return (
+    <div className="max-w-6xl mx-auto px-1 py-10">
+      {/* TITLE */}
+      <h1 className="text-4xl font-bold leading-tight">
+        {post[0].title}
+      </h1>
+
+      {/* META */}
+      <div className="flex items-center gap-2 text-gray-500 mt-3 mb-8 text-sm">
+        <CalendarDays className="w-4 h-4" />
+        <span>{formattedDate}</span>
+      </div>
+
+      {/* CONTENT */}
+      <Renderer content={content} />
+    </div>
+  );
+}
+
+
+
+
+
+
