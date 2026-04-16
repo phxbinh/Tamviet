d0ec41932380b830e958f47ab5c9d61a256ee800
phxbinh <phxbinh@gmail.com>
2026-04-16 22:19:29 +0700

    Create actionServer.ts

---
+++ src/app/(public)/cart/actionServer.ts
@@ -0,0 +1,20 @@
+"use server"
+import { revalidatePath } from "next/cache";
+import { updateCartItem, deleteCartItem } from "@/lib/cart/sqlCart";
+
+export async function updateCartAction(formData: FormData) {
+  const variantId = formData.get("variantId") as string;
+  const quantity = parseInt(formData.get("quantity") as string);
+  if (!variantId || isNaN(quantity)) return;
+
+  await updateCartItem(variantId, quantity);
+  revalidatePath("/cart"); 
+}
+
+export async function removeCartItemAction(formData: FormData) {
+  const variantId = formData.get("variantId") as string;
+  if (!variantId) return;
+
+  await deleteCartItem(variantId);
+  revalidatePath("/cart");
+}
