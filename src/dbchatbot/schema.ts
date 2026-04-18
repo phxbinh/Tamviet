3d245630aaf2d4f8e9d6861f6cd09568dc380249
phxbinh <phxbinh@gmail.com>
2026-04-18 23:15:34 +0700

    Update schema.ts

--- src/dbchatbot/schema.ts
+++ src/dbchatbot/schema.ts
@@ -8,7 +8,10 @@ export const companyPolicies = pgTable("company_policies", {
   
   // Cột chứa tọa độ Vector. 
   // 1536 là số chiều tiêu chuẩn của model text-embedding-3-small từ OpenAI.
-  embedding: vector("embedding", { dimensions: 1536 }),
+  //embedding: vector("embedding", { dimensions: 1536 }),
+  
+  // Dùng với gemini
+  embedding: vector("embedding", { dimensions: 768 }),
   
   // Thông tin bổ sung (tên file, chương, ngày cập nhật...)
   metadata: text("metadata"), 
