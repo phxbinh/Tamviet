3b4e7179252d8e9543c6b3aa692126c8de3726fd
phxbinh <phxbinh@gmail.com>
2026-04-19 14:47:06 +0700

    Update schema.ts

--- src/dbchatbot/schema.ts
+++ src/dbchatbot/schema.ts
@@ -1,28 +1,10 @@
+// src/dbchatbot/schema.ts
+
 import { pgTable, uuid, text, vector } from "drizzle-orm/pg-core";
 
-/*
-export const companyPolicies = pgTable("company_policies", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  
-  // Nội dung văn bản của chính sách (ví dụ: "Nhân viên được nghỉ phép 12 ngày...")
-  content: text("content").notNull(),
-  
-  // Cột chứa tọa độ Vector. 
-  // 1536 là số chiều tiêu chuẩn của model text-embedding-3-small từ OpenAI.
-  embedding: vector("embedding", { dimensions: 1536 }),
-    
-  // Thông tin bổ sung (tên file, chương, ngày cập nhật...)
-  metadata: text("metadata"), 
-});
-*/
-
-// src/dbchatbot/schema.ts
-//import { pgTable, uuid, text, vector } from "drizzle-orm/pg-core";
-
 export const companyPolicies = pgTable("company_policies", {
   id: uuid("id").primaryKey().defaultRandom(),
   content: text("content").notNull(),
-  // Sửa dimensions thành 768 ở đây
   embedding: vector("embedding", { dimensions: 3072 }), 
   metadata: text("metadata"),
 });
