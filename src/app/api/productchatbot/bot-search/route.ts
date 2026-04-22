e721b5bb4d5e300ecca31a51439498fca3bac005
phxbinh <phxbinh@gmail.com>
2026-04-22 23:07:10 +0700

    Create route.ts

---
+++ src/app/api/productchatbot/bot-search/route.ts
@@ -0,0 +1,35 @@
+// services/searchProducts.ts
+
+import { db } from "@/productchatbot";
+import { productDocuments } from "@/productchatbot/schema";
+import { embed } from "ai";
+import { google } from "@ai-sdk/google";
+import { sql, desc } from "drizzle-orm";
+import { cosineDistance } from "drizzle-orm";
+
+export async function searchProducts(query: string) {
+  const embeddingRes = await embed({
+    model: google.embedding("gemini-embedding-001"),
+    value: query,
+  });
+
+  const similarity = sql<number>`
+    1 - (${cosineDistance(
+      productDocuments.embedding,
+      embeddingRes.embedding
+    )})
+  `;
+
+  const results = await db
+    .select({
+      title: productDocuments.title,
+      slug: productDocuments.slug,
+      content: productDocuments.content,
+      score: similarity,
+    })
+    .from(productDocuments)
+    .orderBy(desc(similarity))
+    .limit(5);
+
+  return results;
+}
