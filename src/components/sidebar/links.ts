49096d1bcda98185f35c5c202ad363b6dbdad824
phxbinh <phxbinh@gmail.com>
2026-05-08 16:07:52 +0700

    Update links.ts

--- src/components/sidebar/links.ts
+++ src/components/sidebar/links.ts
@@ -26,6 +26,7 @@ export const sidebarLinks: SidebarLink[] = [
 
   { name: 'Bot sell', href: '/chatbot-sell', showInSidebar: true },
   { name: 'Bot sell - v1', href: '/chatbot-sell-v1', showInSidebar: true },
+  { name: 'Bot sell - v2', href: '/chatbot-sell-v2', showInSidebar: true },
 
   {
     name: 'Environments',
