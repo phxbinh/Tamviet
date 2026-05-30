5ed6d054991a70d3d2067bb90992b273761fba68
phxbinh <phxbinh@gmail.com>
2026-05-30 13:12:45 +0700

    Update AerotankReportView_.tsx

--- src/app/(public)/wastewater/BodRemoveNitrification/AerotankReportView_.tsx
+++ src/app/(public)/wastewater/BodRemoveNitrification/AerotankReportView_.tsx
@@ -412,7 +412,7 @@ $$
 Tính toán phân tách lượng sinh khối hình thành từ tế bào vi khuẩn dị dưỡng dị hóa hữu cơ, phần bùn xác mảnh vỡ tế bào nội bào trơ và vi khuẩn tự dưỡng nitơ hóa:
 
 $$
-P_{\\text{bio}} = \\frac{Q \\cdot Y \\cdot [(COD)_b_{\\text{in}} - (COD)_b_e]}{1 + k_d \\cdot SRT} + \\frac{f_d \\cdot k_d \\cdot Q \\cdot Y \\cdot [(COD)_b_{\\text{in}} - (COD)_b_e] \\cdot SRT}{1 + k_d \\cdot SRT} + \\frac{Q \\cdot Y_n \\cdot (NH_4\\text{-}N)_{\\text{in}}}{1 + k_{dn} \\cdot SRT}
+P_{\\text{bio}} = \\frac{Q \\cdot Y \\cdot [{(COD)_b}_{\\text{in}} - {(COD)_b}_e]}{1 + k_d \\cdot SRT} + \\frac{f_d \\cdot k_d \\cdot Q \\cdot Y \\cdot [{(COD)_b}_{\\text{in}} - {(COD)_b}_e] \\cdot SRT}{1 + k_d \\cdot SRT} + \\frac{Q \\cdot Y_n \\cdot (NH_4\\text{-}N)_{\\text{in}}}{1 + k_{dn} \\cdot SRT}
 $$
 
 * Sinh khối vi khuẩn dị dưỡng hoạt tính phát sinh: **${(c.p_bio_hetero / 1000).toFixed(2)}** $kg\\text{ VSS}/d$
