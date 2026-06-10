db028d5cebf17ab953654ca5e38e078ac8514110
phxbinh <phxbinh@gmail.com>
2026-06-10 17:44:33 +0700

    Create db.ts

---
+++ src/chatbot_OM/services/assets/db.ts
@@ -0,0 +1,16 @@
+import { neon } from "@neondatabase/serverless";
+
+import { drizzle } from "drizzle-orm/neon-http";
+
+import * as schema from "../../schemas";
+
+const sql = neon(
+  process.env.DATABASE_URL!
+);
+
+export const db = drizzle(
+  sql,
+  {
+    schema,
+  }
+);
