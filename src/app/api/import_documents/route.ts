910f61ce3371b311dde3ad708c9b81960df50679
phxbinh <phxbinh@gmail.com>
2026-06-11 14:03:06 +0700

    Update route.ts

--- src/app/api/import_documents/route.ts
+++ src/app/api/import_documents/route.ts
@@ -1,4 +1,4 @@
-/*
+
 import { NextRequest, NextResponse } from "next/server";
 
 import { insertDocument } from "@/chatbot_OM/importDocumentTree_Doc";
@@ -50,4 +50,3 @@ export async function POST(
     );
   }
 }
-*/
