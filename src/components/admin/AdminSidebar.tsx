596b7ca36a41357a8f0d2c40583295bcd3b8aab2
phxbinh <phxbinh@gmail.com>
2026-06-11 14:21:06 +0700

    Update AdminSidebar.tsx

--- src/components/admin/AdminSidebar.tsx
+++ src/components/admin/AdminSidebar.tsx
@@ -66,6 +66,7 @@ export default function AdminSidebar({ user, onNavigate }: AdminSidebarProps) {
     { icon: Box, label: 'productchatbot', href: '/admin/chatbotproduct' },
     { icon: Box, label: 'Tạo cross sell Category', href: '/admin/chatbotproduct/category-cross-sell' },
     { icon: Box, label: 'Guide OM', href: '/admin/operation-guide' },
+    { icon: Box, label: 'import Doc OM', href: '/admin/import_document_om' },
 
   ];
 //src/app/(app)/admin/chatbotproduct/category-cross-sell
